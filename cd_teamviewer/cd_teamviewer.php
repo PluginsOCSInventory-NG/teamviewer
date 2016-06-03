e751c6505c479831c8c7e5c98764431abfe69023
Valentin Deville <MyTheValentinus@users.noreply.github.com>
2016-06-03 09:52:10 +0200

    Update mention

--- cd_teamviewer/cd_teamviewer.php
+++ cd_teamviewer/cd_teamviewer.php
@@ -1,9 +1,12 @@
 <?php
 /*
 ====================================================================================
- OCS INVENTORY
- Copyleft Valentin DEVILLE
+ OCS INVENTORY PLUGINS
+ Copyleft Valentin DEVILLE 2016 
  Web: http://www.ocsinventory-ng.org
+ 
+This code is open source and may be copied and modified as long as the source code is always made freely available.
+Please refer to the General Public Licence http://www.gnu.org/ or LiCENSE.MD
 ====================================================================================
 */
 
