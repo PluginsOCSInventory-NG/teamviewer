9455b0c8997059e402c64abfd1ef2fc0fc24d15e
Gilles Dubois <gillesdubois@users.noreply.github.com>
2017-09-20 12:46:51 +0200

    Update vesion number

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 function plugin_version_teamviewer()
 {
 return array('name' => 'teamviewer',
-'version' => '1.0',
+'version' => '1.1',
 'author'=> 'Valentin DEVILLE',
 'license' => 'GPLv2',
 'verMinOcs' => '2.2');
